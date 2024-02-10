3e146a5392a2b24be44b1e864750a9d7cb879e91
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-02-10 18:54:37 -0500

    chore: Added global main settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //* Setting global prefix
+  app.setGlobalPrefix('/api');
+
+  //* Setting global validation for DTO
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  //* Setting global serialization
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
   await app.listen(3000);
 }
 bootstrap();
