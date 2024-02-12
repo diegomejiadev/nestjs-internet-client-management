3445d11cb7cc923d1e968448362666f3b1ce5f42
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-02-12 13:06:18 -0500

    feat: Injecting user in Request at the JWT guard

--- src/core/guards/jwt-auth.guard.ts
+++ src/core/guards/jwt-auth.guard.ts
@@ -2,10 +2,14 @@ import { Injectable, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { IS_PUBLIC_KEY } from '../decorators/jwt-public.decorator';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private reflector: Reflector) {
+  constructor(
+    private reflector: Reflector,
+    private jwtService: JwtService,
+  ) {
     super();
   }
 
@@ -17,6 +21,18 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     if (isPublic) {
       return true;
     }
+
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    const payload = this.jwtService.decode(token);
+    // 💡 We're assigning the payload to the request object here
+    // so that we can access it in our route handlers
+    request['user'] = payload;
     return super.canActivate(context);
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers['authorization']?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 }
