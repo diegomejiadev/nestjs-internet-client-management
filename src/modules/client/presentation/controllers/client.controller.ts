8aff7aeb24b8de6f268aa60ec6f5ebf7c8e89a71
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-02-12 13:07:11 -0500

    feat: Mandatory JWT in Client controllers

--- src/modules/client/presentation/controllers/client.controller.ts
+++ src/modules/client/presentation/controllers/client.controller.ts
@@ -1,20 +1,17 @@
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { CreateClientDto } from '../../domain/dto/create-client.dto';
 import { ClientService } from '../services/client.service';
-import { Public } from 'src/core/decorators/jwt-public.decorator';
 import { ListClientDto } from '../../domain/dto/list-client.dto';
 
 @Controller('client')
 export class ClientController {
   constructor(private clientService: ClientService) {}
 
-  @Public()
   @Get()
   list(@Query() query: ListClientDto) {
     return this.clientService.listClients(query);
   }
 
-  @Public()
   @Post()
   create(@Body() body: CreateClientDto) {
     return this.clientService.createClient(body);
