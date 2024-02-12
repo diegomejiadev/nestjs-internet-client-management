6247cade8ea4c7a217741663fd108218e7e418ef
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-02-12 10:58:01 -0500

    chore: Added logging in Prisma

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -1,9 +1,31 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient<
+    Prisma.PrismaClientOptions,
+    'query' | 'info' | 'warn' | 'error'
+  >
+  implements OnModuleInit
+{
+  constructor() {
+    super({
+      log: [
+        { emit: 'event', level: 'query' },
+        { emit: 'stdout', level: 'info' },
+        { emit: 'stdout', level: 'warn' },
+        { emit: 'stdout', level: 'error' },
+      ],
+    });
+  }
+
   async onModuleInit() {
+    this.$on('query', (event: Prisma.QueryEvent) => {
+      console.log('Query: ' + event.query);
+      console.log('Variables value: ' + event.params);
+      console.log('Duration: ' + event.duration + 'ms');
+    });
     await this.$connect();
   }
 }
